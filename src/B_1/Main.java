c229152374ed219969a61212a7a41c8e96439cb1
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-01-04 16:18:58 +0300

    B_1 - task added console input

--- src/B_1/Main.java
+++ src/B_1/Main.java
@@ -8,19 +8,76 @@
 
 package B_1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Flowers flowers = new Flowers();
+        Scanner scanner = new Scanner(System.in);
+        int selection = scanner.nextInt();
 
-        flowers.addFlower("Rose", 30, 3, 15);
-        flowers.addFlower("Tulip", 30, 3, 15);
-        flowers.addFlower("Rose", 10, 3, 15);
-        flowers.addFlower("Knapweed", 10, 2, 2);
-        flowers.addFlower("Oregano", 12, 2, 2);
-        flowers.addFlower("Clower", 11, 2, 2);
+        while (selection != 99) {
+            System.out.println("выберите действие : " +
+                    "1-добавить цветок; " +
+                    "2-вывести все добавленные цветы; " +
+                    "3-сортировать по длинне стебля; " +
+                    "4-сортировать по уровню свежести; " +
+                    "99-выйти из программы; ");
+            selection = scanner.nextInt();
 
-        flowers.printAllFlowers();
-        flowers.sortFlowersByStemLength(12, 31);
-        flowers.sortFlowersByFreshnessLevel(3);
+            switch (selection) {
+                case 1: {
+                    System.out.println("Введите через пробел 'Название цветка', " +
+                            "'Длинну стебля - цифру', " +
+                            "'уровень свежести - цифру', " +
+                            "'цену' - цифру");
+                    Scanner in1 = new Scanner(System.in);
+                    String name = in1.next();
+                    int stemLength = in1.nextInt();
+                    int freshnessLevel = in1.nextInt();
+                    int price = in1.nextInt();
+                    flowers.addFlower(name, stemLength, freshnessLevel, price);
+                    break;
+                }
+                case 2: {
+                    flowers.printAllFlowers();
+                    break;
+                }
+                case 3: {
+                    System.out.println("Введите через пробел 'Длинну стебля От..' | 'длинну стебля до..'");
+                    Scanner in3 = new Scanner(System.in);
+                    int stemLengthFrom = in3.nextInt();
+                    int stemLengthTo = in3.nextInt();
+                    flowers.sortFlowersByStemLength(stemLengthFrom, stemLengthTo);
+                    break;
+                }
+                case 4: {
+                    System.out.println("Введите уровень свежести");
+                    Scanner in4 = new Scanner(System.in);
+                    int freshnessLevel = in4.nextInt();
+                    flowers.sortFlowersByFreshnessLevel(freshnessLevel);
+                    break;
+                }
+                case 99: {
+                    System.out.println("Bye bye");
+                    break;
+                }
+                default: {
+                    System.out.println("Вы ввели неверный параметр");
+                    break;
+                }
+            }
+        }
+//        flowers.addFlower(name, stemLength, freshnessLevel, price);
+//        flowers.addFlower("Rose", 30, 3, 15);
+//        flowers.addFlower("Tulip", 30, 3, 15);
+//        flowers.addFlower("Rose", 10, 3, 15);
+//        flowers.addFlower("Knapweed", 10, 2, 2);
+//        flowers.addFlower("Oregano", 12, 2, 2);
+//        flowers.addFlower("Clower", 11, 2, 2);
+
+//        flowers.printAllFlowers();
+//        flowers.sortFlowersByStemLength(12, 31);
+//        flowers.sortFlowersByFreshnessLevel(3);
     }
 }
