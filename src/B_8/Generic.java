8ec75b8c55f846df875563b6498b947a82b22cf7
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-03-25 03:44:04 +0300

    B_8 create generic method2

--- src/B_8/Generic.java
+++ src/B_8/Generic.java
@@ -1,5 +1,6 @@
 package B_8;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class Generic {
@@ -9,5 +10,41 @@ public class Generic {
             result += object.getCountOfPassengers();
         }
         return result;
+    }
+
+    public <T> void sumOfBaggage2(ArrayList<T> objects) throws NoSuchFieldException, IllegalAccessException {
+        int passengersCapacity = 0;
+        int comfortLevel = 0;
+        int countOfBaggage = 0;
+        for (T object : objects) {
+            Class<?> unknownClass = object.getClass();
+            Class<?> parentClass = unknownClass.getSuperclass();
+            Field[] fields = parentClass.getDeclaredFields();
+            for (Field field : fields) {
+                String fieldName = field.getName();
+                Field carriagesField = parentClass.getDeclaredField(fieldName);
+                carriagesField.setAccessible(true);
+                ArrayList<?> arrayListOfObjects = (ArrayList<?>) carriagesField.get(object);
+                for (Object obj : arrayListOfObjects) {
+                    Class<?> objClass = obj.getClass();
+                    Field[] objField = objClass.getDeclaredFields();
+                    for (Field objInnerField : objField) {
+                        objInnerField.setAccessible(true);
+                        // Здесь уже сдался. Не знал как получить поля по отдельности другим способом
+                        if (objInnerField.getName().equals("passengersCapacity")) {
+                            passengersCapacity += (int) objInnerField.get(obj);
+                        } else if (objInnerField.getName().equals("comfortLevel")) {
+                            comfortLevel += (int) objInnerField.get(obj);
+                        } else {
+                            countOfBaggage += (int) objInnerField.get(obj);
+                        }
+                    }
+                }
+            }
+        }
+        System.out.printf("""
+                Generic_2 - Общее число пассажиров всех поездов: %s
+                Generic_2 - Сумма уровней комфорта всех поездов: %s
+                Generic_2 - Общее число багажа: %s""", passengersCapacity, comfortLevel, countOfBaggage);
     }
 }
--- src/B_8/Main.java
+++ src/B_8/Main.java
@@ -10,15 +10,15 @@ package B_8;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         PassengerTrain passengerTrain = new PassengerTrain();
         CargoTrain cargoTrain = new CargoTrain();
         passengerTrain.addVagons(new RailwayCarriege(3, 1));
         passengerTrain.addVagons(new RailwayCarriege(2, 2));
         passengerTrain.addVagons(new RailwayCarriege(1, 3));
         cargoTrain.addVagons(new RailwayCarriege(3, 1, 50));
-        cargoTrain.addVagons(new RailwayCarriege(2, 2, 40));
-        cargoTrain.addVagons(new RailwayCarriege(1, 3, 30));
+        cargoTrain.addVagons(new RailwayCarriege(2, 8, 40));
+        cargoTrain.addVagons(new RailwayCarriege(1, 3, 34));
         passengerTrain.sortVagonsByComfort();
         cargoTrain.sortVagonsByComfort();
         passengerTrain.sortVagonsByPassengersCapacity();
@@ -30,7 +30,9 @@ public class Main {
         listOfTrains.add(passengerTrain);
         listOfTrains.add(cargoTrain);
         Generic generic = new Generic();
-        System.out.println("Общая численность багажа всех поездов" + generic.sumOfBaggage(listOfTrains));
+        System.out.println("Generic_1 - Общая численность пассажиров всех поездов " + generic.sumOfBaggage(listOfTrains));
+
+        generic.sumOfBaggage2(listOfTrains);
     }
 }
 
